2340317af0d3fbaf50f20e31bf53d237ef375b51
chaolei.fu <1310866178@qq.com>
2022-07-20 23:22:57 +0800

    21p

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -14,40 +14,40 @@
 # Testcase Example:  '[1,2,4]\n[1,3,4]'
 #
 # 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
-# 
-# 
-# 
+#
+#
+#
 # 示例 1：
-# 
-# 
+#
+#
 # 输入：l1 = [1,2,4], l2 = [1,3,4]
 # 输出：[1,1,2,3,4,4]
-# 
-# 
+#
+#
 # 示例 2：
-# 
-# 
+#
+#
 # 输入：l1 = [], l2 = []
 # 输出：[]
-# 
-# 
+#
+#
 # 示例 3：
-# 
-# 
+#
+#
 # 输入：l1 = [], l2 = [0]
 # 输出：[0]
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 提示：
-# 
-# 
+#
+#
 # 两个链表的节点数目范围是 [0, 50]
-# -100 
+# -100
 # l1 和 l2 均按 非递减顺序 排列
-# 
-# 
+#
+#
 #
 
 # @lc code=start
@@ -56,7 +56,31 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# 迭代
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         head = ListNode(0)
+#         cur = head
+#         while list1 and list2:
+#             if list1.val > list2.val:
+#                 cur.next = list2
+#                 list2 = list2.next
+#             else:
+#                 cur.next = list1
+#                 list1 = list1.next
+#             cur = cur.next
+#         cur.next = list1 if list1 else list2
+#         return head.next
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 == None:
+            return list2
+        elif list2 == None:
+            return list1
+        elif list1.val > list2.val:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+        list1.next = self.mergeTwoLists(list1.next, list2)
+        return list1
 # @lc code=end
-
