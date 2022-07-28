e5cc4e7e40c5b411e614beff1cc2175ca1fa9252
fuchaolei <chaolei.fu@kuwo.cn>
2022-07-28 21:08:25 +0800

    160

--- 88.合并两个有序数组.go
+++ 88.合并两个有序数组.go
@@ -74,14 +74,22 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	p1 := m - 1
 	p2 := n - 1
 	cur := 0
-	for p1!=-1 || p2!=-1 {
+	for p1 != -1 || p2 != -1 {
 		if p2 == -1 {
-			cur = nums1[p1--]	
+			cur = nums1[p1]
+			p1--
+		} else if p1 == -1 {
+			cur = nums2[p2]
+			p2--
+		} else if nums1[p1] > nums2[p2] {
+			cur = nums1[p1]
+			p1--
+		} else {
+			cur = nums2[p2]
+			p2--
 		}
-		else if p1 == -1 {
-			CUR = NUMMS2[P2--]
-		}
-		else if nums1[p1]
+		nums1[tail] = cur
+		tail--
 	}
 }
 
