e078889c19b2a5fa1a7b09e1b2c08e25fb400ea3
fuchaolei <chaolei.fu@kuwo.cn>
2022-08-01 22:56:43 +0800

    94

--- 94.二叉树的中序遍历.cpp
+++ 94.二叉树的中序遍历.cpp
@@ -14,43 +14,43 @@
  * Testcase Example:  '[1,null,2,3]'
  *
  * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：root = [1,null,2,3]
  * 输出：[1,3,2]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：root = []
  * 输出：[]
- * 
- * 
+ *
+ *
  * 示例 3：
- * 
- * 
+ *
+ *
  * 输入：root = [1]
  * 输出：[1]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 树中节点数目在范围 [0, 100] 内
  * -100 <= Node.val <= 100
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
- * 
+ *
  */
 
 // @lc code=start
@@ -65,11 +65,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+//递归
+// class Solution {
+// public:
+//     vector<int> res;
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         if(root == nullptr){
+//             return res;
+//         }
+//         inorderTraversal(root->left);
+//         res.emplace_back(root->val);
+//         inorderTraversal(root->right);
+//         return res;
+//     }
+// };
+//迭代
+class Solution
+{
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> res;
+        stack<TreeNode *> st;
+        TreeNode *cur = root;
+        while (cur || !empty(st))
+        {
+            if (cur)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            else
+            {
+                cur = st.top();
+                res.emplace_back(cur->val);
+                st.pop();
+                cur = cur->right;
+            }
+        }
+        return res;
     }
 };
 // @lc code=end
-
