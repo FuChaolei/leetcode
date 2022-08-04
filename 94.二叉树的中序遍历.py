a2499e31e9cab953bbab47f9f1dcfc6fff12f9f2
fuchaolei <chaolei.fu@kuwo.cn>
2022-08-04 21:06:46 +0800

    232

--- 94.二叉树的中序遍历.py
+++ 94.二叉树的中序遍历.py
@@ -14,43 +14,43 @@
 # Testcase Example:  '[1,null,2,3]'
 #
 # 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
-# 
-# 
-# 
+#
+#
+#
 # 示例 1：
-# 
-# 
+#
+#
 # 输入：root = [1,null,2,3]
 # 输出：[1,3,2]
-# 
-# 
+#
+#
 # 示例 2：
-# 
-# 
+#
+#
 # 输入：root = []
 # 输出：[]
-# 
-# 
+#
+#
 # 示例 3：
-# 
-# 
+#
+#
 # 输入：root = [1]
 # 输出：[1]
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 提示：
-# 
-# 
+#
+#
 # 树中节点数目在范围 [0, 100] 内
 # -100 <= Node.val <= 100
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
-# 
+#
 #
 
 # @lc code=start
@@ -60,18 +60,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    res = []
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if root ==None:
-            return self.res
-        cur = root
-        self.inorderTraversal(cur.left)
-        self.res.append(cur.val)
-        self.inorderTraversal(cur.right)
-        return self.res
-            
 # class Solution:
+#     res = []
 #     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+#         def inorder(root):
+#             if root ==None:
+#                 return res
+#             cur = root
+#             inorder(cur.left)
+#             res.append(cur.val)
+#             inorder(cur.right)
+#         inorder(root)
+#         return res
+# 迭代
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        st = []
+        res = []
+        cur = root
+        while cur != None or len(st) > 0:
+            if cur != None:
+                st.append(cur)
+                cur = cur.left
+            else:
+                cur = st[-1]
+                res.append(cur.val)
+                st = st[:-1]
+                cur = cur.right
+        return res
 # @lc code=end
-
