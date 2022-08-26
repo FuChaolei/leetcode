d810a995880183ba61f21eb157bc193327e75114
fuchaolei <chaolei.fu@kuwo.cn>
2022-08-26 21:37:10 +0800

    11

--- 283.移动零.cpp
+++ 283.移动零.cpp
@@ -14,33 +14,36 @@
  * Testcase Example:  '[0,1,0,3,12]'
  *
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
- * 
+ *
  * 示例:
- * 
+ *
  * 输入: [0,1,0,3,12]
  * 输出: [1,3,12,0,0]
- * 
+ *
  * 说明:
- * 
- * 
+ *
+ *
  * 必须在原数组上操作，不能拷贝额外的数组。
  * 尽量减少操作次数。
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    void moveZeroes(vector<int>& nums) {
-        int n = numms.size(), left = 0, right = 0;
-        int r = nums.size() - 1;
-        while (right < n){
-            if(nums[left]==0)
+    void moveZeroes(vector<int> &nums)
+    {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++)
         {
-            swap(nums[left],)
-        }}
+            if (nums[i])
+            {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
     }
 };
 // @lc code=end
-
