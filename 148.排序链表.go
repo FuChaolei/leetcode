56db7a5e07215b0f77bf49b27638c2b322932de7
fuchaolei <chaolei.fu@kuwo.cn>
2022-10-14 21:32:59 +0800

    148

--- 148.排序链表.go
+++ 148.排序链表.go
@@ -102,7 +102,7 @@
 // }
 
 type pair struct {
-	first *ListNode
+	first  *ListNode
 	second *ListNode
 }
 
@@ -110,13 +110,81 @@ func sortList(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
-}
-func merge(l1 *ListNode, l2 *ListNode) *pair
-{
-	dummy := &ListNode{
-		Val:0,
-		Next:nil,
+	cur := head
+	len := 0
+	for cur != nil {
+		len++
+		cur = cur.Next
 	}
+	//fmt.Print(len)
+	var l *ListNode
+	var r *ListNode
+	var tail *ListNode
+	dummy := &ListNode{
+		Val:  0,
+		Next: head,
+	}
+	for i := 1; i < len; i <<= 1 {
+		cur = dummy.Next
+		tail = dummy
+		for cur != nil {
+			l = cur
+			r = split(l, i)
+			cur = split(r, i)
+			merged := merge(l, r)
+			tail.Next = merged.first
+			tail = merged.second
+		}
+	}
+	return dummy.Next
 }
+func split(head *ListNode, n int) *ListNode {
+
+	for n > 1 && head != nil {
+		n--
+		head = head.Next
+	}
+	var res *ListNode
+	if head != nil {
+		res = head.Next
+	} else {
+		res = nil
+	}
+	if head != nil {
+		head.Next = nil
+	}
+	return res
+}
+func merge(l1 *ListNode, l2 *ListNode) *pair {
+	//fmt.Print(l1.Val,l2.Val)
+	dummy := &ListNode{
+		Val:  0,
+		Next: nil,
+	}
+	cur := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val > l2.Val {
+			l1, l2 = l2, l1
+		}
+		cur.Next = l1
+		l1 = l1.Next
+		cur = cur.Next
+	}
+	if l1 != nil {
+		cur.Next = l1
+	}
+	if l2 != nil {
+		cur.Next = l2
+	}
+	for cur.Next != nil {
+		cur = cur.Next
+	}
+	res := &pair{
+		first:  dummy.Next,
+		second: cur,
+	}
+	return res
+}
+
 // @lc code=end
 
