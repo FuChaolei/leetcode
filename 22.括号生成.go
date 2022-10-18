afd964159f19d3abaf81822c61dfda44eeab1eef
chaolei.fu <1310866178@qq.com>
2022-10-18 23:28:33 +0800

    93

--- 22.括号生成.go
+++ 22.括号生成.go
@@ -14,36 +14,56 @@
  * Testcase Example:  '3'
  *
  * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：n = 3
  * 输出：["((()))","(()())","(())()","()(())","()()()"]
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：n = 1
  * 输出：["()"]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= n <= 8
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 func generateParenthesis(n int) []string {
-
+	s := ""
+	res := []string{}
+	var dfs func(string, int, int)
+	dfs = func(s string, l int, r int) {
+		if l == 0 && r == 0 {
+			res = append(res, s)
+			return
+		}
+		if r < l {
+			return
+		}
+		if l > 0 {
+			dfs(s+"(", l-1, r)
+		}
+		if r > 0 {
+			dfs(s+")", l, r-1)
+		}
+	}
+	dfs(s, n, n)
+	return res
 }
+
 // @lc code=end
 
