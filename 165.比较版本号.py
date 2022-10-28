56c935b140b54c085b9ee7fa96fe37f692a014e0
fuchaolei <1310866178@qq.com>
2022-10-28 22:33:42 +0800

    165

--- 165.比较版本号.py
+++ 165.比较版本号.py
@@ -74,6 +74,24 @@
 # @lc code=start
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
+        m,n = len(version1),len(version2)
+        i,j=0,0
+        x,y=0,0
+        while i<m or j<n:
+            x,y=0,0
+            while i<m and version1[i]!='.':
+                x = x*10 + (ord(version1[i])-ord('0'))
+                i+=1
+            i+=1
+            while j<n and version2[j]!='.':
+                y = y*10 + (ord(version2[j])-ord('0'))
+                j+=1
+            j+=1
+            if x<y:
+                return -1
+            if x>y:
+                return 1
+        return 0
 
 # @lc code=end
 
