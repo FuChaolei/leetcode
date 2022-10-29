3c2ae61d3acac34a1d52f2aa656191ce7c6c8f92
fuchaolei <chaolei.fu@kuwo.cn>
2022-10-29 21:44:09 +0800

    43

--- 43.字符串相乘.cpp
+++ 43.字符串相乘.cpp
@@ -49,8 +49,42 @@ class Solution
 public:
     string multiply(string num1, string num2)
     {
-
-        
+        if (num1 == "0" || num2 == "0")
+        {
+            return "0";
+        }
+        int n = num1.size(), m = num2.size();
+        vector<int> res(m + n, 0);
+        string fin = "";
+        int t = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = m - 1; j >= 0; j--)
+            {
+                res[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+            }
+        }
+        for (int i = m + n - 1; i >= 0; i--)
+        {
+            t += res[i];
+            res[i] = t % 10;
+            t /= 10;
+        }
+        if (res.size() > 1 && res[0] == 0)
+        {
+            for (int i = 1; i < res.size(); i++)
+            {
+                fin += to_string(res[i]);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < res.size(); i++)
+            {
+                fin += to_string(res[i]);
+            }
+        }
+        return fin;
     }
 };
 // @lc code=end
