5a3ab5126d63a6ef3c0e673a54f4a8990fd72ff6
fuchaolei <1310866178@qq.com>
2022-11-08 23:50:24 +0800

    124

--- 124.二叉树中的最大路径和.go
+++ 124.二叉树中的最大路径和.go
@@ -15,37 +15,37 @@
  *
  * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个
  * 节点，且不一定经过根节点。
- * 
+ *
  * 路径和 是路径中各节点值的总和。
- * 
+ *
  * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：root = [1,2,3]
  * 输出：6
  * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
- * 
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：root = [-10,9,20,null,null,15,7]
  * 输出：42
  * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 树中节点数目范围是 [1, 3 * 10^4]
- * -1000 
- * 
- * 
+ * -1000
+ *
+ *
  */
 
 // @lc code=start
@@ -58,7 +58,25 @@
  * }
  */
 func maxPathSum(root *TreeNode) int {
-
+	res:=math.MinInt32
+	maxS(root,&res)
+	return res
+}
+func maxS(root *TreeNode,res *int)int{
+	if root==nil {
+		return 0
+	}
+	l:=max(0,maxS(root.Left,res))
+	r:=max(0,maxS(root.Right,res))
+	sum := l+r+root.Val
+	*res := max(*res,num)
+	return max(l,r)+root.Val
+}
+func max(a int,b int)int{
+	if a>b{
+		return a
+	}
+	return b
 }
 // @lc code=end
 
