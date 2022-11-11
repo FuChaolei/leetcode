596ab171ec851ca1fa0034132c8695f643d71644
fuchaolei <chaolei.fu@kuwo.cn>
2022-11-11 21:40:27 +0800

    72

--- 72.编辑距离.cpp
+++ 72.编辑距离.cpp
@@ -62,6 +62,45 @@
 
 // @lc code=start
 //递归
+// class Solution
+// {
+// public:
+//     int minDistance(string word1, string word2)
+//     {
+//         int l1 = word1.size();
+//         int l2 = word2.size();
+//         dp = vector<vector<int>>(l1 + 1, vector<int>(l2 + 1, -1));
+//         return minDistance(word1, word2, l1, l2);
+//     }
+
+// private:
+//     vector<vector<int>> dp;
+//     int minDistance(string &word1, string &word2, int l1, int l2)
+//     {
+//         if (l1 == 0)
+//         {
+//             return l2;
+//         }
+//         if (l2 == 0)
+//         {
+//             return l1;
+//         }
+//         if (dp[l1][l2] >= 0)
+//         {
+//             return dp[l1][l2];
+//         }
+//         int ans;
+//         if (word1[l1 - 1] == word2[l2 - 1])
+//         {
+//             ans = minDistance(word1, word2, l1 - 1, l2 - 1);
+//         }
+//         else
+//         {
+//             ans = 1 + min(minDistance(word1, word2, l1 - 1, l2 - 1), min(minDistance(word1, word2, l1 - 1, l2), minDistance(word1, word2, l1, l2 - 1)));
+//         }
+//         return dp[l1][l2] = ans;
+//     }
+// };
 class Solution
 {
 public:
@@ -69,59 +108,8 @@ public:
     {
         int l1 = word1.size();
         int l2 = word2.size();
-        dp = vector<vector<int>>(l1 + 1, vector<int>(l2 + 1, -1));
-        minDistance(word1, word2, l1, l2);
-        for (int i = 0; i < l1; i++)
-        {
-            for (int j = 0; j < l2; j++)
-            {
-                cout << dp[i][j] << " ";
-            }
-            cout << endl;
-        }
-        return dp[l1][l2];
+        vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1, -1));
     }
 
-private:
-    vector<vector<int>> dp;
-    int minDistance(string &word1, string &word2, int l1, int l2)
-    {
-        if (l1 == 0)
-        {
-            // dp[l1][l2] = l2;
-            return l2;
-        }
-        if (l2 == 0)
-        {
-            // dp[l1][l2] = l1;
-            return l1;
-        }
-        if (dp[l1][l2] >= 0)
-        {
-            return dp[l1][l2];
-        }
-        int ans;
-        if (word1[l1 - 1] == word2[l2 - 1])
-        {
-            ans = minDistance(word1, word2, l1 - 1, l2 - 1);
-        }
-        else
-        {
-            ans = 1 + min(minDistance(word1, word2, l1 - 1, l2 - 1), min(minDistance(word1, word2, l1 - 1, l2), minDistance(word1, word2, l1, l2 - 1)));
-            if (l1 == l2 && l1 == 1)
-            {
-                cout << ans;
-            }
-        }
-        return dp[l1][l2] = ans;
-    }
 };
-// class Solution {
-// public:
-//     int minDistance(string word1, string word2) {
-//         int l1 = word1.size();
-//         int l2 = word2.size();
-//         vector<vector<int>> dp(l1+1,vector<int>(l2+1,-1))
-//     }
-// };
 // @lc code=end
