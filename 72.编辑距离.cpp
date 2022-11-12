7c3e24b92e9965bdf6303bba8e5c9211ec8ac586
chaolei.fu <1310866178@qq.com>
2022-11-12 23:50:57 +0800

    72

--- 72.编辑距离.cpp
+++ 72.编辑距离.cpp
@@ -109,15 +109,37 @@ public:
         int l1 = word1.size();
         int l2 = word2.size();
         vector<vector<int>> dp(l1 + 1, vector<int>(l2 + 1, -1));
-        for (int i = 0; i <= l1;i++){
+        for (int i = 0; i <= l1; i++)
+        {
             dp[i][0] = i;
         }
         for (int i = 0; i <= l2; i++)
         {
-            dp
+            dp[0][i] = i;
         }
-
+        for (int i = 1; i <= l1; i++)
+        {
+            for (int j = 1; j <= l2; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+        // for (int i = 0; i <= l1; i++)
+        // {
+        //     for (int j = 0; j <= l2; j++)
+        //     {
+        //         cout << dp[i][j] << " ";
+        //     }
+        //     cout << endl;
+        // }
+        return dp[l1][l2];
     }
-
 };
 // @lc code=end
