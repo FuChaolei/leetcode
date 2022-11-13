82a5973254d9b0ce76bcaed2971c1c3ffda5b2c6
fuchaolei <1310866178@qq.com>
2022-11-13 12:59:58 +0800

    72

--- 72.编辑距离.go
+++ 72.编辑距离.go
@@ -65,7 +65,35 @@
 
 // }
 func minDistance(word1 string, word2 string) int {
-
+	l1 := len(word1)
+	l2 := len(word2)
+	dp := make([][]int, l1+1)
+	for i := 0; i <= l1; i++ {
+		dp[i] = make([]int, l2+1)
+	}
+	for i := 0; i <= l1; i++ {
+		dp[i][0] = i
+	}
+	for i := 0; i <= l2; i++ {
+		dp[0][i] = i
+	}
+	for i := 1; i <= l1; i++ {
+		for j := 1; j <= l2; j++ {
+			if word1[i-1] == word2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1
+			}
+		}
+	}
+	return dp[l1][l2]
 }
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
 // @lc code=end
 
