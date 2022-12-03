4346b0b166d2959fa415f1c392604f5886d4ef8f
fuchaolei <1310866178@qq.com>
2022-12-03 14:42:08 +0800

    239

--- 239.滑动窗口最大值.cpp
+++ 239.滑动窗口最大值.cpp
@@ -15,14 +15,14 @@
  *
  * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k
  * 个数字。滑动窗口每次只向右移动一位。
- * 
+ *
  * 返回 滑动窗口中的最大值 。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1：
- * 
- * 
+ *
+ *
  * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
  * 输出：[3,3,5,5,6,7]
  * 解释：
@@ -34,33 +34,81 @@
  * ⁠1  3  -1 [-3  5  3] 6  7       5
  * ⁠1  3  -1  -3 [5  3  6] 7       6
  * ⁠1  3  -1  -3  5 [3  6  7]      7
- * 
- * 
+ *
+ *
  * 示例 2：
- * 
- * 
+ *
+ *
  * 输入：nums = [1], k = 1
  * 输出：[1]
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  * 提示：
- * 
- * 
+ *
+ *
  * 1 <= nums.length <= 10^5
  * -10^4 <= nums[i] <= 10^4
  * 1 <= k <= nums.length
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
-class Solution {
+// class Solution
+// {
+// public:
+//     vector<int> maxSlidingWindow(vector<int> &nums, int k)
+//     {
+//         priority_queue<pair<int, int>> q;
+//         for (int i = 0; i < k; i++)
+//         {
+//             q.emplace(nums[i], i);
+//         }
+//         vector<int> res{q.top().first};
+//         for (int i = k; i < nums.size(); i++)
+//         {
+//             q.emplace(nums[i], i);
+//             while (q.top().second <= i - k)
+//             {
+//                 q.pop();
+//             }
+//             res.emplace_back(q.top().first);
+//         }
+//         return res;
+//     }
+// };
+class Solution
+{
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-
+    vector<int> maxSlidingWindow(vector<int> &nums, int k)
+    {
+        deque<int> q;
+        vector<int> res;
+        for (int i = 0; i < k; i++)
+        {
+            while (!q.empty() && nums[q.back()] <= nums[i])
+            {
+                q.pop_back();
+            }
+            q.emplace_back(i);
+        }
+        res.emplace_back(nums[q.front()]);
+        for (int i = k; i < nums.size(); i++)
+        {
+            while (!q.empty() && q.front() <= i - k)
+            {
+                q.pop_front();
+            }
+            while (!q.empty() && nums[q.back()] <= nums[i])
+            {
+                q.pop_back();
+            }
+            q.emplace_back(i);
+            res.emplace_back(nums[q.front()]);
+        }
+        return res;
     }
 };
 // @lc code=end
-
